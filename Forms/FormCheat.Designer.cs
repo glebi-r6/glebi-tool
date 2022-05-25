83a1dff3c8cd2a935a70840aa1285d2b95fd7919
glebi-r6 <84925457+glebi-r6@users.noreply.github.com>
2022-05-25 19:33:32 +0200

    Add files via upload

---
+++ Forms/FormCheat.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace glebi_tool_2te_design.Forms
+{
+    partial class FormDiscord
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBasic = new System.Windows.Forms.Button();
+            this.btnRust = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnBasic
+            // 
+            this.btnBasic.BackColor = System.Drawing.Color.Red;
+            this.btnBasic.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnBasic.FlatAppearance.BorderSize = 0;
+            this.btnBasic.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBasic.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBasic.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnBasic.Location = new System.Drawing.Point(29, 133);
+            this.btnBasic.Name = "btnBasic";
+            this.btnBasic.Size = new System.Drawing.Size(216, 41);
+            this.btnBasic.TabIndex = 14;
+            this.btnBasic.Text = "Basic NoRecoil";
+            this.btnBasic.UseVisualStyleBackColor = false;
+            this.btnBasic.Click += new System.EventHandler(this.btnBasic_Click);
+            // 
+            // btnRust
+            // 
+            this.btnRust.BackColor = System.Drawing.Color.Red;
+            this.btnRust.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.btnRust.FlatAppearance.BorderSize = 0;
+            this.btnRust.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRust.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRust.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnRust.Location = new System.Drawing.Point(29, 65);
+            this.btnRust.Name = "btnRust";
+            this.btnRust.Size = new System.Drawing.Size(216, 41);
+            this.btnRust.TabIndex = 15;
+            this.btnRust.Text = "Rust NoRecoil";
+            this.btnRust.UseVisualStyleBackColor = false;
+            this.btnRust.Click += new System.EventHandler(this.btnRust_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(25, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 22);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "No Recoil";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(25, 199);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 22);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "More ?";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.button1.Location = new System.Drawing.Point(29, 245);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(216, 41);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Suggestions";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.button2.Location = new System.Drawing.Point(29, 313);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(216, 41);
+            this.button2.TabIndex = 17;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Visible = false;
+            // 
+            // FormDiscord
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(33)))), ((int)(((byte)(74)))));
+            this.ClientSize = new System.Drawing.Size(862, 536);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRust);
+            this.Controls.Add(this.btnBasic);
+            this.Name = "FormDiscord";
+            this.Text = "Cheats";
+            this.Load += new System.EventHandler(this.FormDiscord_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBasic;
+        private System.Windows.Forms.Button btnRust;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ Forms/FormCheat.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace glebi_tool_2te_design.Forms
+{
+    public partial class FormDiscord : Form
+    {
+        public FormDiscord()
+        {
+            InitializeComponent();
+        }
+        
+        private void FormDiscord_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRust_Click(object sender, EventArgs e)
+        {
+            var formPopup = new Recoil.Form1();
+            formPopup.Show(this);
+        }
+
+        private void btnBasic_Click(object sender, EventArgs e)
+        {
+            var formPopup = new NoRecoil.gui();
+            formPopup.Show(this);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://discord.gg/xqpRDHtznw");
+        }
+    }
+    
+}
